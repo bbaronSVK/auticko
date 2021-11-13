9654d3c2aab5cd32aeb7cd42643e76d2e92bbe9c
Michal Paulovic <coolmip@gmail.com>
2021-11-13 16:11:07 +0100

    Počiatočné súbory pre projekt MakeCode

---
+++ main.ts
@@ -0,0 +1,33 @@
+input.onSound(DetectedSound.Loud, function () {
+    ledky = 1
+    while (!(input.logoIsPressed())) {
+        RingbitCar.freestyle(randint(-100, 100), randint(-10, 10))
+        basic.pause(200)
+        RingbitCar.running_time(RingbitCar.Direction_run.forward, 1)
+    }
+    ledky = 0
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    ledky = 0
+})
+let ledky = 0
+input.setSoundThreshold(SoundThreshold.Loud, 160)
+ledky = 0
+let strip = neopixel.create(DigitalPin.P0, 2, NeoPixelMode.RGB)
+strip.clear()
+RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    if (ledky == 1) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        music.ringTone(349)
+        basic.pause(500)
+        music.ringTone(494)
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+        basic.pause(500)
+    } else {
+        music.stopAllSounds()
+        strip.clear()
+        RingbitCar.brake()
+    }
+})
