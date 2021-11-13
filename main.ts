cfdd70b47ebbadf5e1f7ace62ffe5c07a02c76ce
Michal Paulovic <coolmip@gmail.com>
2021-11-13 16:46:26 +0100

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,38 @@
 input.onSound(DetectedSound.Loud, function () {
-    ledky = 1
+    vystraseny = 1
     while (!(input.logoIsPressed())) {
-        RingbitCar.freestyle(randint(-100, 100), randint(-10, 10))
-        basic.pause(500)
+        RingbitCar.freestyle(randint(-255, 255), randint(-255, 255))
+        basic.pause(200)
     }
-    ledky = 0
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    ledky = 0
+    vystraseny = 0
+    basic.clearScreen()
 })
-let ledky = 0
-input.setSoundThreshold(SoundThreshold.Loud, 160)
-ledky = 0
-let strip = neopixel.create(DigitalPin.P0, 2, NeoPixelMode.RGB)
-strip.clear()
+let vystraseny = 0
+input.setSoundThreshold(SoundThreshold.Loud, 180)
+vystraseny = 0
+let pasik = neopixel.create(DigitalPin.P0, 2, NeoPixelMode.RGB)
+pasik.clear()
 RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
-basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.QuarterNote)
+pasik.showRainbow(1, 360)
+basic.pause(1000)
+basic.showIcon(IconNames.EigthNote)
 basic.forever(function () {
-    if (ledky == 1) {
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    if (vystraseny == 1) {
+        pasik.showColor(neopixel.colors(NeoPixelColors.Red))
         music.ringTone(349)
-        basic.pause(500)
+        basic.showIcon(IconNames.Sad)
+        basic.pause(200)
         music.ringTone(494)
-        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
-        basic.pause(500)
+        pasik.showColor(neopixel.colors(NeoPixelColors.Blue))
+        basic.showIcon(IconNames.Surprised)
+        basic.pause(200)
     } else {
+        basic.showIcon(IconNames.Yes)
         music.stopAllSounds()
-        strip.clear()
+        pasik.showColor(neopixel.colors(NeoPixelColors.White))
         RingbitCar.brake()
     }
 })
